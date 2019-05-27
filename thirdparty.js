91e17994b80ddc34f48bf969003606ae122fb370
ssemrau <szymon.semrau@gmail.com>
2019-05-27 10:53:09 +0200

    test2

--- thirdparty.js
+++ thirdparty.js
@@ -1,5 +1,7 @@
 ﻿
 setTimeout(function () {
     var iframe1 = document.getElementById('the_iframe_1');
+    var third_party = document.getElementById('third_party');
+    third_party.innerHTML = "1";
     console.log(iframe1);
 }, 1000);
